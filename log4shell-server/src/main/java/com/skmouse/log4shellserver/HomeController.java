58e338feb926ff40a503849e04cdaf5f873af45d
김승기 <57786933+SeungkeyMouse@users.noreply.github.com>
2022-01-19 00:37:48 +0900

    실습
    postman을 사용하여 오류 확인

---
+++ log4shell-server/src/main/java/com/skmouse/log4shellserver/HomeController.java
@@ -0,0 +1,17 @@
+package com.skmouse.log4shellserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping("/")
+    public String index(String name){
+        log.debug("index! name:{}", name);
+        return "index";
+    }
+}
